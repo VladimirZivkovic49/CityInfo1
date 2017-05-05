e4ac8fdac6dc70342442456318e221fc25bac06a
VladimirZivkovic49 <vlaja432@gmail.com>
2017-05-05 17:25:11 +0200

    Revert "4 commit"
    This reverts commit 7d5fa1a83fa4a40ca7e8a8cb4632d2d4c9e32481.

--- src/CityInfo1/Entities/PointOfInterest.cs
+++ src/CityInfo1/Entities/PointOfInterest.cs
@@ -17,11 +17,6 @@ namespace CityInfo1.Entities
         [MaxLength(50)]
         public string Name { get; set; }
 
-        [MaxLength(200)]
-
-        public string Description { get; set; }
-
-
         [ForeignKey("CityId")]
         public City City { get; set; }
         public int CityId { get; set; }
